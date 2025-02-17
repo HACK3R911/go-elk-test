b4a51aef3c9e6bbe203ab30676b7b24fca5f0041
HACK3R911 <afanasiev-2004@list.ru>
2025-02-17 18:33:09 +0300

    add init db+conf struct

--- db/database.go
+++ db/database.go
@@ -1,5 +1,40 @@
-package main
+package db
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"github.com/rs/zerolog"
+)
 
+type Database struct {
+	Conn   *sql.DB
+	Logger zerolog.Logger
+}
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DbName   string
+	Logger   zerolog.Logger
+}
+
+// DB connection
+func Init(cfg Config) (Database, error) {
+	db := Database{}
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		cfg.Host, cfg.Port, cfg.Username, cfg.Password, cfg.DbName)
+	conn, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return db, err
+	}
+
+	db.Conn = conn
+	db.Logger = cfg.Logger
+	err = db.Conn.Ping()
+	if err != nil {
+		return db, err
+	}
+	return db, nil
 }
